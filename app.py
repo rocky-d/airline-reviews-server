2a337db9aaf99ea6d39883a3046c14abb06c5c09
Rocky Haotian Du <2712479005@qq.com>
2023-09-26 17:26:20 +0800

    Update /*.py

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ rc = StrictRedis(connection_pool = get_rp())
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        param1 = request.form.get('param1')
-        param2 = request.form.get('param2')
-        param3 = request.form.get('param3')
-        param4 = request.form.get('param4')
-        param5 = request.form.get('param5')
-        param6 = request.form.get('param6')
-        param7 = request.form.get('param7')
-        param8 = request.form.get('param8')
-        param9 = request.form.get('param9')
-
-        filename = f'{param1}_{param2}_{param3}x{param4}_from{param5}_to{param6}'
-        path = r'static/' + filename
-        generate_word_cloud(get_reviews_for_airline(rc, param1), filename)
-        return render_template('index.html', result = path)
+        # param1 = request.form.get('param1')
+        # param2 = request.form.get('param2')
+        # param3 = request.form.get('param3')
+        # param4 = request.form.get('param4')
+        # param5 = request.form.get('param5')
+        # param6 = request.form.get('param6')
+        # param7 = request.form.get('param7')
+        # param8 = request.form.get('param8')
+        # param9 = request.form.get('param9')
+        #
+        # filename = f'{param1}_{param2}_{param3}x{param4}_from{param5}_to{param6}'
+        # path = r'static/' + filename
+        # generate_word_cloud(get_reviews_for_airline(rc, param1), path)
+        return render_template('index.html', result = r'static/wordcloud.png')
     else:
         return render_template('index.html', result = 'waiting')
 
--- utils.py
+++ utils.py
@@ -30,26 +30,26 @@ def update_redis_list(redis_client: Redis, list_key: str, new_list: list[str]) -
         redis_client.rpush(list_key, row)
 
 
-def get_reviews_for_airline(redis_client: Redis, airl_name: str) -> str | None:
+def get_reviews_for_airline(redis_client: Redis, airl_name: str) -> str:
+    res = ''
+
     airl_name = airl_name.lower()
     df = pd.read_csv(r'data_csv/AIRLINEREVIEWS_DB/AIRLINE.csv')
+    # for arl_json in iter_redis_list(redis_client, 'AIRLINE'):
+    #     arl_dict = json.loads(arl_json)
     for index, row in df.iterrows():
         arl_dict = row.to_dict()
-        # for arl_json in iter_redis_list(redis_client, 'AIRLINE'):
-        #     arl_dict = json.loads(arl_json)
         if airl_name == arl_dict['ARL_NAME'].lower():
             airl_iata = arl_dict['ARL_IATA']
             break
     else:
-        return None
-
-    res = ''
+        return 'AIRLINE_NOT_FOUND'
 
     df = pd.read_csv(r'data_csv/AIRLINEREVIEWS_DB/REVIEW.csv')
+    # for rev_json in iter_redis_list(redis_client, 'REVIEW'):
+    #     rev_dict = json.loads(rev_json)
     for index, row in df.iterrows():
         rev_dict = row.to_dict()
-        # for rev_json in iter_redis_list(redis_client, 'REVIEW'):
-        #     rev_dict = json.loads(rev_json)
         if airl_iata == rev_dict['REV_FLT_ARL_IATA']:
             res += rev_dict['REV_TITLE'] + ' ' + rev_dict['REV_CONTENT'] + ' '
 
@@ -62,7 +62,7 @@ def get_reviews_for_airline(redis_client: Redis, airl_name: str) -> str | None:
             .strip())
 
 
-def generate_word_cloud(text: str, filename: str) -> None:
+def generate_word_cloud(text: str, path: str, width: int = 1920, height: int = 1080, bc: str = 'white') -> None:
     # 假设你有一个大的字符串变量 text
     # 这里简单地使用空格来拆分文本为单词，你可以根据需要进行更复杂的文本处理
     words = text.lower().split()  # 将文本拆分成单词列表
@@ -71,7 +71,10 @@ def generate_word_cloud(text: str, filename: str) -> None:
 
     for word in words:
         # 使用字典来计算单词的频率
-        word_freq[word] = word_freq.get(word, 0) + 1
+        if word in ['good', 'bad', 'awesome', 'happy', 'nice', 'worst', 'mad', 'angry', 'best']:
+            word_freq[word] = word_freq.get(word, 0) + 5
+        else:
+            word_freq[word] = word_freq.get(word, 0) + 1
 
     # 常见的无关紧要的单词列表
     common_words_to_exclude = [
@@ -84,7 +87,7 @@ def generate_word_cloud(text: str, filename: str) -> None:
         'our', 'very', 'my', 'me', 'too', 'if', 'didn', 'did', 'a', 'b', 'c',
         'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
         'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'his', 'her', 'mine', 'its',
-        'about', 'some', 'thing', 'because', 'been', 'being'
+        'about', 'some', 'thing', 'because', 'been', 'being', 'don'
     ]  # 添加更多你认为不需要包含在词云中的单词
 
     # 移除无关紧要的单词
@@ -92,15 +95,15 @@ def generate_word_cloud(text: str, filename: str) -> None:
                           if word not in common_words_to_exclude}
 
     # 创建一个WordCloud对象并生成词云图像
-    wordcloud = WordCloud(width = 1920, height = 1080, background_color = 'white')
+    wordcloud = WordCloud(width = width, height = height, background_color = bc)
     wordcloud.generate_from_frequencies(filtered_word_freq)
 
     # 保存词云图像到当前目录
-    wordcloud.to_file(r'static\wordcloud.png')
+    wordcloud.to_file(path)
 
     print('wordcloud.png saved')
 
 
 if __name__ == '__main__':
     rc = StrictRedis(connection_pool = get_rp())
-    generate_word_cloud(get_reviews_for_airline(rc, 'Air Canada rouge'), f'')
+    generate_word_cloud(get_reviews_for_airline(rc, 'air canada'), f'static/wordcloud.png')
