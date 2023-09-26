66b798085c673c5a23e01bfbfd14f06bad173e5f
charlesyifanli <charlesyifanli@gmail.com>
2023-09-26 17:26:23 +0800

    Update app.py index.html

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # 初始化变量
-result = ""
+result = {
+    'input_text1': '',
+    'input_text2': '',
+    'input_text3': '',
+    'input_text4': '',
+    'input_text5': '',
+    'input_text6': ''
+}
 image_path = "static/line_plot.png"
 
 
@@ -16,10 +23,19 @@ def index():
 # 处理表单提交
 @app.route('/process', methods=['POST'])
 def process():
+    global image_path
     global result  # 使用全局变量
     # 获取用户输入的信息
-    input_text = request.form['input_text']
-    result = input_text  # 更新全局变量
+    result['input_text1'] = request.form['input_text1']
+    result['input_text2'] = request.form['input_text2']
+    result['input_text3'] = request.form['input_text3']
+    result['input_text4'] = request.form['input_text4']
+    result['input_text5'] = request.form['input_text5']
+    result['input_text6'] = request.form['input_text6']
+    #
+    if request.form['input_text6'] == '1':
+        image_path = 'static/1.png'
+    #
     return render_template('index.html', result=result, image_path=image_path)
 
 
