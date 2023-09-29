2af1aeeaa9efc3edc043efcea108e108a25aefb5
Rocky Haotian Du <2712479005@qq.com>
2023-09-29 19:41:17 +0800

    Update /*

--- takeoff.bat
+++ takeoff.bat
@@ -1,6 +1,7 @@
 @echo off
 
+cd C:\rocky_d\code\databases\dmd-presentation
 start "Airline Reviews Visualization" "http://localhost:63342/dmd-presentation/templates/redirect.html"
-python.exe app.py
+C:\rocky_d\code\databases\dmd-presentation\dmd-presentation\Scripts\python.exe app.py
 
 exit
